103e45eaa9f98c11ec7008a28408ef09306ca6b0
deepakchauhanxai-widget <deepakchauhanxai@gmail.com>
2025-11-15 15:39:39 +0530

    Update community-wall-box.js

--- scripts/community-wall-box.js
+++ scripts/community-wall-box.js
@@ -253,7 +253,7 @@ class ProfessionalCommunityBox {
 // Open Community Wall function
 function openCommunityWall() {
     const currentLang = localStorage.getItem('preferredLanguage') || 'en';
-    const communityWallUrl = `/community-wall.html?lang=${currentLang}`;
+    const communityWallUrl = `https://deepakchauhanxai.xyz/testing-dk/community-wall.html?lang=${currentLang}`;
     
     console.log('🚀 Opening community wall:', communityWallUrl);
     window.open(communityWallUrl, '_blank');
